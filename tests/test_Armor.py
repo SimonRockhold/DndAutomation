aebf53f74f6297061110313bdc1e344a08fd9ba9
Simon Rockhold <s@rockhold.co>
2021-07-14 20:49:28 -0700

    updated test_Armor and added more tests

--- tests/test_Armor.py
+++ tests/test_Armor.py
@@ -1,34 +1,61 @@
-from PlayerClass import Artificer
+from Character import Character
+from PlayerClass import PlayerClass, Artificer
 from Equipment import Armor
 from Infusion import ArmorInfusion
 import unittest
 
 class test(unittest.TestCase):
     def setUp(self):
-        self.c = Artificer(12, 16, 15, 20, 14, 11, proficiencyBonus=3, health=38)
-        self.c.maxNumInfusions = 6
-        scaleMail = Armor('Scale Mail', 'it\'s some pretty neat armor', 14, 'medium')
-        self.c.addToInventory(scaleMail)
-        self.enhancedDefence = ArmorInfusion('Enhanced Defence', 'plus 1', scaleMail)
-        self.c.learnInfusion(self.enhancedDefence)
+        self.c = Character(12, 20, 15, 20, 14, 11, level=6, playerClass=Artificer(), maxHealth=38)
+        self.c.addProficiencies('martial weapons', 'firearms')
+        self.c.playerClass.maxNumInfusions = 6
         
-        return
-
     def tearDown(self):
         del self.c
-        del self.enhancedDefence
 
-    def test_armor(self):
-
-        self.c.don('Scale Mail')
-        assert self.c.armorClass == 16, 'when unenhanced, armor should be 16'
-        self.c.prepareInfusion(self.enhancedDefence)
-        assert self.c.armorClass == 17, 'when enhanced, armor should be 17'
+    def test_light_armor(self):
+        leatherArmor = Armor('Leather Armor', 'some simple light armor', 11, 'light')
+        self.c.addToInventory(leatherArmor)
+        self.c.don('Leather Armor')
+        assert self.c.armorClass == 16, '10 + dexmod = 16'
         self.c.doff()
-        assert self.c.armorClass == 13, 'when no armor equiped, AC should equal 13'
-        print('All armor tests passed')
-        return
 
+    def test_medium_armor(self):
+        chainShirt = Armor('Chain Shirt', 'it\'s some pretty neat armor', 13, 'medium')
+        self.c.addToInventory(chainShirt)
+        self.c.don('Chain Shirt')
+        assert self.c.armorClass == 15, '13 + dexMod(5) (max 2)'
+        self.c.doff()
+        self.c.abilityScores['dex'] = (12, 1) #dexMod of 1
+        self.c.don('Chain Shirt')
+        assert self.c.armorClass == 14, '13 + dexMod(1) (max 2)'
+        self.c.doff()
+
+
+    def test_heavy_armor(self):
+        # TODO when implemented, test whether a character not proficient in heavy armor is correctly debuffed
+        plateArmor = Armor('Plate Armor', 'heavy armor test', 18, 'heavy')
+        self.c.addToInventory(plateArmor)
+        self.c.don('Plate Armor')
+        assert self.c.armorClass == 18, 'dex mod should not be applied to heavy armor'
+        self.c.doff()
+
+    def test_unarmoredAC(self):
+        assert self.c.armorClass == 15, 'no armor donned, AC should equal 10 + dexBonus(5)'
+
+
+    def test_armor_infusion(self):
+        
+        scaleMail = Armor('Scale Mail', 'decent medium armor', 14, 'medium')
+        self.c.addToInventory(scaleMail)
+        self.enhancedDefence = ArmorInfusion('Enhanced Defence', 'plus 1', scaleMail)
+        self.c.playerClass.learnInfusions(self.enhancedDefence)
+        self.c.don('Scale Mail')
+        assert self.c.armorClass == 16, 'when unenhanced, armor should be 16(10 + min(dexMod, 3)'
+        self.c.playerClass.prepareInfusion(self.enhancedDefence, self.c.inventory)
+        assert self.c.armorClass == 17, 'when enhanced, armor should be AC(16) + 1'
+        self.c.doff()
+        assert self.c.armorClass == 15, 'when no armor equipped, AC should equal unarmoredAC'
 
 if __name__ == '__main__':
     unittest.main()
